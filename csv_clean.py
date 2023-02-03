1bfb0e24327df4969a52494fedb2623e99a41753
Gabriel Rodríguez <gabrc52@gmail.com>
2023-02-03 13:55:53 -0500

    python clean

---
+++ csv_clean.py
@@ -0,0 +1,29 @@
+import csv
+import os
+import json
+
+csv_path = "downloaded/social_explorer"
+csv_output_path = "social_explorer_cleaned"
+
+csv_files = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
+
+names = {}
+
+def clean(sheet: list[list]):
+    assert len(sheet[0]) == len(sheet[1])
+    N = len(sheet[0])
+    for i in range(N):
+        names[sheet[1][i]] = sheet[0][i]
+    sheet.pop(0)
+
+for filename in csv_files:
+    with open(f'{csv_path}/{filename}', 'r') as f:
+        sheet = list(csv.reader(f))
+    
+    clean(sheet)
+
+    with open(f'{csv_output_path}/{filename}', 'w') as f:
+        csv.writer(f).writerows(sheet)
+
+with open('names.json', 'w') as f:
+    json.dump(names, f)
